d311c719a3ff220808d9c6d2020371829b3f0b53
chelloo87 <chelloo_octa_87@yahoo.com>
2016-10-25 12:44:37 +0300

    Update install_on_server.sh
    update script

--- install_on_server.sh
+++ install_on_server.sh
@@ -3,7 +3,6 @@ sudo apt-get update
 
 sudo apt-get install nodejs
 
-#npm install express --save
 npm install express-generator -g
 
 express -hbs light
@@ -16,8 +15,6 @@ sudo service nginx start
 
 cd /etc/nginx/
 sudo rm site-enabled/default
-
-# Change the directory to sites-available and create a new file called Ghost using the nano editor
 
 cd sites-available
 sudo nano light
